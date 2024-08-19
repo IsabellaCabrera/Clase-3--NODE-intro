a0fd7ed36573b6f92dc9fb479a9c21736a389f9e
Isa cabrera.S <158528449+IsabellaCabrera@users.noreply.github.com>
2024-08-18 22:30:50 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+import {sum , multiply} from './utilsMath.js'
+// aca ya estoy importando la funcion.
+
+const value = sum (  4 , 2);
+console.log(value)
---
+++ utilsMath.js
@@ -0,0 +1,14 @@
+export const sum =( a, b) => {
+    return a + b; 
+}; //modulo- funcion que voy a reutilizar en otro archivo 
+
+ export const multiply = ( a, b) =>{
+    return a * b
+};// otro modulo 
+
+//para exportar ambos modulos
+
+// module.export = {
+//     sum,
+//     multiply,
+// };esto de la manera vieja 
